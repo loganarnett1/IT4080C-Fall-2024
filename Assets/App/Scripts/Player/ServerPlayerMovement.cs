48d4da66abe43d26188b50beffc0400d194bf95f
loganarnett1 <59770778+loganarnett1@users.noreply.github.com>
2024-12-04 02:18:13 -0500

    Added animations to server player

--- Assets/App/Scripts/Player/ServerPlayerMovement.cs
+++ Assets/App/Scripts/Player/ServerPlayerMovement.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEditor.ShortcutManagement;
 using UnityEngine.EventSystems;
+using System.Linq;
+using Unity.Netcode.Components;
+using TMPro;
 
 namespace App.Scripts.Player
 {
     public class ServerPlayerMovement : NetworkBehaviour
     {
         [SerializeField] private float movementSpeed;
+        [SerializeField] private float movementSpeedRunning;
         [SerializeField] private Transform playerTransform;
+        [SerializeField] private NetworkAnimator networkAnimator;
+        [SerializeField] private Animator playerAnimator;
 
         public CharacterController characterController;
 
@@ -27,25 +33,37 @@ namespace App.Scripts.Player
             if (!IsOwner) return;
             
             Vector2 moveInput = playerInput.Player.Movement.ReadValue<Vector2>();
+
+            bool isJumping = playerInput.Player.Jump.triggered;
+            bool isPunching = playerInput.Player.Punch.triggered;
+            bool isSprinting = playerInput.Player.Sprint.ReadValue<float>() > 0;
             
             if (IsServer)
             {
-                Move(moveInput, Time.deltaTime);
+                Move(moveInput, Time.deltaTime, isJumping, isPunching, isSprinting);
             }
             else if (IsClient) 
             {
-                MoveServerRPC(moveInput, Time.deltaTime);
+                MoveServerRPC(moveInput, Time.deltaTime, isJumping, isPunching, isSprinting);
             }
         }
 
-        private void Move(Vector2 input, float deltaTime)
+        private void Move(Vector2 input, float deltaTime, bool isJumping, bool isPunching, bool isSprinting)
         {
             Vector3 movementDirection = input.x * playerTransform.right + input.y * playerTransform.forward;
 
-            characterController.Move(movementDirection * movementSpeed * deltaTime);
+            bool isWalking = input.x != 0 || input.y != 0;
+
+            playerAnimator.SetBool("IsWalking", isWalking);
+            playerAnimator.SetBool("IsSprinting", isSprinting && isWalking);
+            if (isJumping) networkAnimator.SetTrigger("JumpTrigger");
+            if (isPunching) networkAnimator.SetTrigger("PunchTrigger");
+
+            characterController.Move(movementDirection * deltaTime * (isSprinting ? movementSpeedRunning : movementSpeed));
         }
 
         [Rpc(target:SendTo.Server)]
-        private void MoveServerRPC(Vector2 input, float deltaTime) => Move(input, deltaTime);
+        private void MoveServerRPC(Vector2 input, float deltaTime, bool isJumping, bool isPunching, bool isSprinting) =>
+            Move(input, deltaTime, isJumping, isPunching, isSprinting);
     }
 }
